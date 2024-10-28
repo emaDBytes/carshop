ec037968f6d1a3edf719f087ac24624b85a634e5
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-28 21:41:14 +0200

    Update carapi.js

--- src/carapi.js
+++ src/carapi.js
@@ -1,15 +1,16 @@
 export function fetchCars() {
   return fetch(import.meta.env.VITE_API_URL).then((response) => {
-    if (!response) throw new Error("Error in fetch: " + response.statusText);
+    if (!response.ok) throw new Error("Error in fetch: " + response.statusText);
 
     return response.json();
   });
 }
 
 export function deleteCar(url) {
-  return fetch(url, { method: "DELETE" })
-    .then(response => {
-        if (!response.ok)
-            throw new Error("Error in delete: " + )
-    })
+  return fetch(url, { method: "DELETE" }).then((response) => {
+    if (!response.ok)
+      throw new Error("Error in delete: " + response.statusText);
+
+    return response.json();
+  });
 }
